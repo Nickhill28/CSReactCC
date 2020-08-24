2eb4b760ae9b5b4e362694b32ef6a609b5a96d9f
Nickhill28 <peddythevolcano@gmail.com>
2020-08-24 17:47:05 +0530

    Updates

---
+++ precache-manifest.aff34e40a8dc536179516b7896e94f71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f344700fd32c6f2b5112af24b081e47",
+    "url": "/Todo-next/index.html"
+  },
+  {
+    "revision": "f2e7531f099b9c28f8e5",
+    "url": "/Todo-next/static/css/main.9a63582d.chunk.css"
+  },
+  {
+    "revision": "df1d391af1a53cc2f42a",
+    "url": "/Todo-next/static/js/2.cd3d5607.chunk.js"
+  },
+  {
+    "revision": "6c9acc89429084ef38352de9865ed65a",
+    "url": "/Todo-next/static/js/2.cd3d5607.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2e7531f099b9c28f8e5",
+    "url": "/Todo-next/static/js/main.56211abb.chunk.js"
+  },
+  {
+    "revision": "8e0ed9c6fab64f34e085",
+    "url": "/Todo-next/static/js/runtime-main.c7c88449.js"
+  },
+  {
+    "revision": "4b58443287b4d1e5185692d7d4cb02a8",
+    "url": "/Todo-next/static/media/usethis.4b584432.jpg"
+  }
+]);
